aae0d8faef707cfa10e72146096185ccedbd35a7
LF <lf@lfxyz.com>
2018-04-09 09:14:01 -0400

    Implement Handlebard parsere

--- compose.js
+++ compose.js
@@ -4,40 +4,55 @@ once no components are unloaded and texts are loaded --> populate texts
 
 */
 (function() {
-const populateTemplate = (template, data) => template.replace(/{([^{}]*)}/g, (a, b) =>  data[b]);
+const pageName = window.location.pathname.split('/').pop().split('.html').join(''); 
+const contentPath = 'content';
+const contentFileName = `${pageName}.json`;
 
+const setupDependencies = () => {
+  const scriptElement = document.createElement('script');
+  scriptElement.setAttribute('src', 'https://cdnjs.cloudflare.com/ajax/libs/handlebars.js/4.0.11/handlebars.min.js');
+  document.head.appendChild(scriptElement);
+  scriptElement.onload = composeHtml;
+};
+  
+const populateContent = () => {
+  const regexQuery = /<\/block>|<block(.*?)>/g;
+  const source = document.body.innerHTML;
+  const filteredSource = source.replace(regexQuery, () => '');
+  const template = Handlebars.compile(filteredSource);
+  fetch(`${contentPath}/${contentFileName}`)
+      .then(data => data.json())
+      .then(content => {
+        const html = template(content);
+        document.body.innerHTML = html;
+      })
+      .catch(err => console.log(err))
+}
+  
+let pendingFetches = 0;
+  
 const composeHtml = () => {
   const fileNameAttributeName = 'src';
   const tagName = 'block';
-  const textAttributeName = 'txt';
   const componentsPath = 'components';
-  const textsPath = 'texts';
   const query = `${tagName}[${fileNameAttributeName}]`;
   const elements = document.querySelectorAll(query);
-
+  
   for (let i = 0; i < elements.length; i++) {
     const element = elements[i];
     const componentFileName = element.getAttribute(fileNameAttributeName);
     element.removeAttribute(fileNameAttributeName);
+    pendingFetches++;
     fetch(`${componentsPath}/${componentFileName}`)
       .then(data => data.text())
       .then(component => {
-        const textFileName = element.getAttribute(textAttributeName);
-        if(textFileName) {
-          fetch(`${textsPath}/${textFileName}`)
-            .then(data => data.json())
-            .then(texts => {
-              const populatedComponent = populateTemplate(component, texts)
-              element.innerHTML = populatedComponent;
-              composeHtml();
-            });
-        } else {
-          element.innerHTML = component;
-          composeHtml();
-        }
+        pendingFetches--;
+        element.innerHTML = component;
+        composeHtml();
       });
   }
+  if(pendingFetches === 0) populateContent();
 };
 
-composeHtml();
+setupDependencies();
 })();
